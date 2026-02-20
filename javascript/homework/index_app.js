15de500dfe9062849e676c6b4349cf846a50cde6
aiLinMc <woyougemimi123@163.com>
2026-02-20 11:48:09 +0800

    test

--- javascript/homework/index_app.js
+++
@@ -1,9 +0,0 @@
-// 检测URL参数并隐藏APP链接
-document.addEventListener('DOMContentLoaded', function() {
-    // 检查URL参数
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get('source') === 'app') {
-        // 如果在APP内运行，隐藏下载链接
-        document.getElementById('app-link').classList.add('hidden');
-    }
-});
